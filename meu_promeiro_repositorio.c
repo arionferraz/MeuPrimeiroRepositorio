a6daea101df15b6a9facefbd193cdea9e9f21bbf
Árion Ferraz <84385090+arionferraz@users.noreply.github.com>
2022-07-11 23:19:48 -0300

    Commit codigo .c

---
+++ meu_promeiro_repositorio.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+	printf("\n\t#########################\n");
+	printf("\tMeu primeiro repositorio!!");
+	printf("\n\t#########################\n");
+	return 0;
+}
